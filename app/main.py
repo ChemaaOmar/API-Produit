90a9577169698bd81ea379e21a8d21df63cc604e
AbdelmalekYounes <abdelmalekyounes31@gmail.com>
2024-06-04 22:41:22 +0200

    database migration to postgre

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
-from .database import engine
-from .models import Base
+from .database import engine, Base
 from .routers import products
 
 app = FastAPI()
 
+
 Base.metadata.create_all(bind=engine)
+
 
 app.include_router(products.router)
