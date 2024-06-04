6ce19c2dee64bbde3b5bc1690bed126902ca1cfd
AbdelmalekYounes <abdelmalekyounes31@gmail.com>
2024-06-04 22:41:53 +0200

    Migration CI/CD

---
+++ migrations/env.py
@@ -0,0 +1,40 @@
+from logging.config import fileConfig
+from sqlalchemy import create_engine
+from sqlalchemy import pool
+from alembic import context
+
+# Ce modèle est votre module qui contient vos modèles SQLAlchemy
+from app.models import Base
+
+# Configuration de la connexion à la base de données
+config = context.config
+
+# Lecture du fichier de configuration `alembic.ini`
+fileConfig(config.config_file_name)
+
+# Définir l'objet `target_metadata` pour les modèles SQLAlchemy
+target_metadata = Base.metadata
+
+# Crée une fonction pour exécuter les migrations
+def run_migrations_offline():
+    url = config.get_main_option("sqlalchemy.url")
+    context.configure(
+        url=url, target_metadata=target_metadata, literal_binds=True, dialect_opts={"paramstyle": "named"}
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+def run_migrations_online():
+    connectable = create_engine(config.get_main_option("sqlalchemy.url"))
+
+    with connectable.connect() as connection:
+        context.configure(connection=connection, target_metadata=target_metadata)
+
+        with context.begin_transaction():
+            context.run_migrations()
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
